9756dca58fda2756bdaad11917ad08e51b6abc92
Mark Evans <mark.evans3@redcross.org>
2018-08-18 19:41:12 -0600

    Adds #show action, error for not found #index and #show

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
-  def index
-    @search_history = last_ten_searches
+  before_action :last_ten_searches, :only => [:index, :show]
 
+  def index
     if params[:query]
       retrieve_or_create_search
       @query_results
@@ -9,30 +9,51 @@ class SearchController < ApplicationController
   end
 
   def show
-
+    @search = retrieve_search_object
   end
 
   private
 
   def last_ten_searches
-    @last_ten_searches =
+    @search_history =
       Search.order(:updated_at).take(10)
   end
 
   def retrieve_or_create_search
     query = params[:query].upcase
-    search_results = Search
-      .find_by(:term => query)
+    search_results = Search.find_by(:term => query)
 
     if search_results
-     coin_data = Binance.coin_price(search_results.term + "USDT")
      search_results.update_attribute(:last_searched, DateTime.now)
      @query_results = search_results.reload
     else
-      @query_results = Search.create(
-        :term => query,
-        :last_searched => DateTime.now
-      )
+      @query_results = create_search_object(query)
     end
+  end
+
+  def create_search_object(query)
+    object = Search.create(
+      :term => query,
+      :last_searched => DateTime.now
+    )
+
+    object.id ? object : not_found_error
+  end
+
+  def retrieve_search_object
+    object = Search
+      .includes(:search_instances)
+      .where(:id => params[:id])
+      .first
+
+    object ? object : not_found_error
+  end
+
+  def not_found_error
+    {
+      :error => 'Either Binance does not pair your searched crypto-coin'\
+        ' with USDT (Tether), or it does not exist.  Please check'\
+        ' the spelling and try again.'
+    }
   end
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -13,10 +13,27 @@ RSpec.describe SearchController, type: :controller do
 
   describe "GET index w/ search params" do
     it "assigns @search_history" do
-
       get :index, :params => { :query => "XRP" }
 
       expect(assigns(:query_results)).to be_present
+    end
+  end
+
+  describe "GET show" do
+    it "returns a search object" do
+      search = Search.create(:term => "XRP")
+
+      get :show, :params => { :id => search.id }
+
+      expect(assigns(:search)).to eq(search)
+    end
+
+    context "not found" do
+      it "returns error message" do
+        get :show, :params => { :id => 0 }
+
+        expect(assigns(:search)[:error]).to be_present
+      end
     end
   end
 end
